835bd7b16e944e3f8d5141f2df80a4aab78ae9fa
Adrien W (Rizz) <laserheart.spectroniztar@gmail.com>
2025-01-03 20:59:20 -0500

    Dont even know if this works bc my bro's PC doesnt run Python :/

--- system/miscellaneous/tests/python/turtle.py
+++ system/miscellaneous/tests/python/turtle.py
@@ -1,4 +1,16 @@
+import tkinter as tk
+
+root = tk.Tk()
+canvas = tk.Canvas(root, width=400, height=300)
+canvas.pack()
+
+background_color = 'black'
+canvas.create_rectangle(0, 0, 400, 300, fill=background_color)
+
+root.mainloop()
+
 from turtle import *
+pencolor('white')
 for i in range(72)
   for j in range(6):
     forward(70)
